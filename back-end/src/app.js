d978bd54b9257eec56c2c656f0df95ee4dfeb401
Nola Dodd <noladodd.code@gmail.com>
2024-06-06 13:06:58 -0700

    production 1

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -12,27 +12,11 @@ const tablesRouter = require("./tables/tables.router")
 
 const app = express();
 
-// var corsOptions = function(req, res, next){ 
-//     res.header('Access-Control-Allow-Origin', req.headers.origin); 
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 
-//     'Content-Type, Authorization, Content-Length, X-Requested-With');
-//      next();
-// }
-
 let corsOptions = { 
     origin : ['*', 'http://localhost:3000'], 
     } 
     
 app.use(cors(corsOptions))
-
-// let allowedOrigins = ["http://ServerA:3000", "http://ServerB:3000"]
-// let origin = req.headers.origin;
-// if (allowedOrigins.includes(origin)) {
-//     res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
-// }
-
-// app.use(corsOptions);
 
 app.use(express.json());
 
