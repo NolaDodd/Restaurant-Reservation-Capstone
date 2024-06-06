315d4cb84cc6b9b0384b019acb278d57c7384511
Nola Dodd <noladodd.code@gmail.com>
2024-06-06 11:24:59 -0700

    CORS 2

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -12,7 +12,16 @@ const tablesRouter = require("./tables/tables.router")
 
 const app = express();
 
-app.use(cors());
+var corsOptions = function(req, res, next){ 
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 
+    'Content-Type, Authorization, Content-Length, X-Requested-With');
+     next();
+}
+
+app.use(corsOptions);
+
 app.use(express.json());
 
 app.use("/reservations", reservationsRouter);
